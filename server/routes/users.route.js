9dc21c513fab82d6bbbe5223aa3bd39db3d4031e
HaruKishima <18120256@student.hcmus.edu.vn>
2021-11-13 10:11:56 +0700

    update user and studentId

--- server/routes/users.route.js
+++ server/routes/users.route.js
@@ -7,9 +7,13 @@ router.get('/', authenticate.verifyUser, controller.getAllUsers);
 router.post('/login', controller.postLogin);
 router.post('/signup', controller.postSignup);
 router.get('/google/token', authenticate.verifyGoogle, controller.socialLogin);
-router.get('/facebook/token', authenticate.verifyFacebook, controller.socialLogin);
+router.get(
+  '/facebook/token',
+  authenticate.verifyFacebook,
+  controller.socialLogin
+);
 
 router.get('/:id', authenticate.verifyUser, controller.getUserById);
-
+router.post(':id', authenticate.verifyUser, controller.updateUser);
 
 module.exports = router;
