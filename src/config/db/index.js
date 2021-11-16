36327451727dbd0a82c19fe133693c338db125b6
NAT <ntuan.2502@gmail.com>
2021-11-16 17:20:52 +0700

    restructure directory (1)

---
+++ src/config/db/index.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+const { MONGODB_URL } = require("../mainConfig");
+async function connect() {
+  try {
+    await mongoose.connect(MONGODB_URL);
+    console.log("Connect to MongoDB on port 27017");
+  } catch (error) {
+    console.log("Error connecting to Mongo: ", error);
+  }
+}
+
+module.exports = { connect };
