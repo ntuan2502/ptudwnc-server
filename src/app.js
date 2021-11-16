5cce0d3f263a3fe10ab82d9a68db3303db31b2ed
NAT <ntuan.2502@gmail.com>
2021-11-16 20:08:29 +0700

    upgrade package, test /auth/register API OK

--- src/app.js
+++ src/app.js
@@ -1,55 +1,47 @@
 require("dotenv").config();
 require("express-async-errors");
-const createError = require("http-errors");
+// const createError = require("http-errors");
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const morgan = require("morgan");
 const passport = require("passport");
 const cors = require("cors");
 
+var methodOverride = require("method-override");
 const app = express();
 const db = require("./config/db");
 const route = require("./routers");
 const port = 3000;
 
+app.use(morgan("combined"));
+app.use(methodOverride("_method"));
+app.use(cors({ origin: "*", optionsSuccessStatus: 200 }));
+app.use(passport.initialize());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cookieParser());
+
+require("./authenticate");
 db.connect();
 route(app);
+
+
+// catch 404 and forward to error handler
+// app.use(function (req, res, next) {
+//   next(createError(404));
+// });
+
+// error handler
+// app.use(function (err, req, res, next) {
+//   // set locals, only providing error in development
+//   res.locals.message = err.message;
+//   res.locals.error = req.app.get("env") === "development" ? err : {};
+
+//   // render the error page
+//   res.status(err.status || 500);
+//   res.render("error");
+// });
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-const corswithOption = {
-  origin: "*",
-  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-};
-
-
-require("./authenticate");
-
-app.use(cors(corswithOption));
-app.use(passport.initialize());
-
-app.use(morgan("dev"));
-app.use(morgan("combined"));
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-
-module.exports = app;
