57e58af4e63c308d35289c184522c4b56162946a
NAT <ntuan.2502@gmail.com>
2021-11-18 08:32:03 +0700

    git rm -rf --cached .

--- src/app.js
+++ src/app.js
@@ -1,6 +1,5 @@
 require('dotenv').config();
 require('express-async-errors');
-// const createError = require("http-errors");
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const morgan = require('morgan');
@@ -11,7 +10,7 @@ var methodOverride = require('method-override');
 const app = express();
 const db = require('./config/db');
 const route = require('./routers');
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8000;
 
 app.use(morgan('combined'));
 app.use(methodOverride('_method'));
@@ -25,22 +24,6 @@ require('./authenticate');
 db.connect();
 route(app);
 
-// catch 404 and forward to error handler
-// app.use(function (req, res, next) {
-//   next(createError(404));
-// });
-
-// error handler
-// app.use(function (err, req, res, next) {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get("env") === "development" ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render("error");
-// });
-
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`App listening at http://localhost:${port}`);
 });
